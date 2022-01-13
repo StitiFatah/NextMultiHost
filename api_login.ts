3d7b4bd86c327010d9344868e94195d6dea0be6d
D_user <ffoot9698@gmail.com>
2022-01-13 21:41:42 +0100

    test domain 6

--- api_login.ts
+++ api_login.ts
@@ -2,7 +2,11 @@ import axios from "axios";
 
 // export const BASE_API_URL = "http://localhost:5000/";
 export const BASE_API_URL1 = "http://localhost:5000";
-export const BASE_API_URL = "https://testdjangowebservice.onrender.com";
+
+export const BASE_API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://testdjangowebservice.onrender.com"
+    : "http://localhost:5000";
 
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
