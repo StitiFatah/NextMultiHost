734effa5967136921bfb165acad5b409b62c9c1b
D_user <ffoot9698@gmail.com>
2022-01-13 20:39:59 +0100

    base api url render

--- api_login.ts
+++ api_login.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const BASE_API_URL = "http://localhost:5000/";
+// export const BASE_API_URL = "http://localhost:5000/";
 export const BASE_API_URL1 = "http://localhost:5000";
+export const BASE_API_URL = "https://testdjangowebservice.onrender.com";
+
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
   timeout: 10000,
