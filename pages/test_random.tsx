94715d75256ea9bf90a1ede35d618ff384c23b65
D_user <ffoot9698@gmail.com>
2022-03-25 01:12:29 +0100

    test usag 2e

--- pages/test_random.tsx
+++ pages/test_random.tsx
@@ -1,7 +1,9 @@
 export default function TestRandom() {
+  const img =
+    "https://imgproxy.caprover.archeroe.xyz/zpZu5WA2oSgl6Nc790vMcjX5wYtdoX1XI6k7IJQaudA/rs:fit:0:0:0/g:ce:0:0/czM6Ly9ibGMvdHV4/Mg.";
   return (
     <>
-      <img src="/tux_wp2.png" />
+      <img src={img} />
     </>
   );
 }
